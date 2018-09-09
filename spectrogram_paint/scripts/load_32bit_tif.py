226407bfcf4efc98593f43f89fcc53123468aac2
Lucas Walter <wsacul@gmail.com>
2018-09-09 08:20:53 -0700

    Loop and reload images and reprocess if different- TODO use sys method to detect if file has changed?

--- spectrogram_paint/scripts/load_32bit_tif.py
+++ spectrogram_paint/scripts/load_32bit_tif.py
@@ -33,10 +33,10 @@ if False:
     nperseg = 1023
     amp = 2 * np.sqrt(2)
     noise_power = 0.001 * fs / 2
-    time = np.arange(N) / float(fs)
-    carrier = amp * np.sin(2*np.pi*50*time)
+    t = np.arange(N) / float(fs)
+    carrier = amp * np.sin(2*np.pi*50*t)
     noise = np.random.normal(scale=np.sqrt(noise_power),
-                             size=time.shape)
+                             size=t.shape)
     x = carrier + noise
 
     f, t, Zxx = signal.stft(x, fs=fs, nperseg=nperseg, return_onesided=onesided)
@@ -46,50 +46,95 @@ if False:
     print np.min(zmag), np.max(zmag)
     print np.min(zphase), np.max(zphase)
 
-mag_image = sys.argv[1]
-phase_image = sys.argv[2]
-im = np.flipud(io.imread(mag_image))
-
-print mag_image, im.shape, np.min(im), np.max(im)
-if len(im.shape) > 2:
-    print 'warning image has multiple layers', im.shape[2]
-    im = im[:,:,0]
-# print im
-
-phase = io.imread(phase_image)
-print phase_image, phase.shape, np.min(phase), np.max(phase)
-
-zxx = im * np.exp(1j * phase)
-
+mag_file = sys.argv[1]
+phase_file = sys.argv[2]
 fs = 44100
-to, x_unfiltered = signal.istft(zxx, fs=fs, input_onesided=onesided)
+loop = True
+second_pass = False
 
-# filter out the DC and the higher frequencies
-xo = butter_bandpass_filter(x_unfiltered, lowcut=20.0, highcut=10000.0, fs=fs, order=3)
+do_bandpass = True
+lowcut = 20
+highcut = 10e3
 
-nperseg = zxx.shape[0] * 2 - 1
-print 'nperseg', nperseg
-f2, t2, zxx2 = signal.stft(xo, fs=fs, nperseg=nperseg, return_onesided=onesided)
-print 'max frequency', np.max(f2)
-print zxx2.shape
+mag = None
+phase = None
+old_mag = None
+old_phase = None
 
-t, x = signal.istft(zxx2, fs=fs, input_onesided=onesided)
+while True:
+    time.sleep(0.3)
 
-zmag = np.abs(zxx2)
-print zmag
-print 'z min max', np.min(zmag), np.max(zmag)
-logzmag = np.log10(zmag + 1e-10)
-logzmag -= np.min(logzmag)
-zangle = np.angle(zxx2)
-# plt.show()
+    start = time.time()
+    mag = np.flipud(io.imread(mag_file))
 
-sp = np.fft.fft(x)
-half_ind = sp.shape[-1] / 2
-sp_mag = np.abs(sp[:half_ind])
-print 'fft min max', np.min(sp_mag), np.max(sp_mag)
-freq = np.fft.fftfreq(t.shape[-1], 1.0 / fs)
-freq = freq[:half_ind]
+    if len(mag.shape) > 2:
+        print 'warning mag image has multiple layers', mag.shape[2]
+        mag = mag[:,:,0]
 
+    phase = np.flipud(io.imread(phase_file))
+    if len(phase.shape) > 2:
+        print 'warning phase image has multiple layers', phase.shape[2]
+        phase = phase[:,:,0]
+    end = time.time()
+    print end - start
+
+    if phase.shape != mag.shape:
+        print phase.shape, '!=', mag.shape
+        continue
+
+    update = old_phase is None or old_mag is None
+    if not update:
+        update = mag.shape != old_mag.shape
+    if not update:
+        update = not np.array_equal(phase, old_phase) or not np.array_equal(mag, old_mag)
+    if not update:
+        continue
+    print 'updating'
+    print 'mag', mag_file, mag.shape, np.min(mag), np.max(mag)
+    print 'phase', phase_file, phase.shape, np.min(phase), np.max(phase)
+    old_mag = mag
+    old_phase = phase
+
+    mag = np.exp(mag * 4) - 1.0
+    zxx = mag * np.exp(1j * phase)
+
+    to, x_unfiltered = signal.istft(zxx, fs=fs, input_onesided=onesided)
+
+    # filter out the DC and the higher frequencies
+    if do_bandpass:
+        xo = butter_bandpass_filter(x_unfiltered, lowcut=lowcut, highcut=highcut, fs=fs, order=3)
+    else:
+        xo = x_unfiltered
+
+    if second_pass:
+        nperseg = zxx.shape[0] * 2 - 1
+        print 'nperseg', nperseg
+        f2, t2, zxx2 = signal.stft(xo, fs=fs, nperseg=nperseg, return_onesided=onesided)
+        print 'max frequency', np.max(f2)
+        print zxx2.shape
+
+        t, x = signal.istft(zxx2, fs=fs, input_onesided=onesided)
+
+        zmag = np.abs(zxx2)
+        print zmag
+        print 'z min max', np.min(zmag), np.max(zmag)
+        logzmag = np.log10(zmag + 1e-10)
+        logzmag -= np.min(logzmag)
+        zangle = np.angle(zxx2)
+    else:
+        t = to
+        x = xo
+
+    x = x / np.max(np.abs(x))
+    wavfile.write("test.wav", fs, x)
+
+if False:
+    sp = np.fft.fft(x)
+    half_ind = sp.shape[-1] / 2
+    sp_mag = np.abs(sp[:half_ind])
+    print 'fft min max', np.min(sp_mag), np.max(sp_mag)
+    freq = np.fft.fftfreq(t.shape[-1], 1.0 / fs)
+    freq = freq[:half_ind]
 
 
 if False:
@@ -105,8 +150,6 @@ if False:
     plt.xlabel('frequency')
     plt.ylabel('magnitude')
 
-x = x / np.max(np.abs(x))
-wavfile.write("test.wav", fs, x)
 
 sind = 40000
 find = sind + 1000
