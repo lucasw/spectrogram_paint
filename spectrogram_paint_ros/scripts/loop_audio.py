d8d0726254057f94fdb4000cb549358d97ebfac8
Lucas Walter <wsacul@gmail.com>
2018-10-11 20:00:40 -0700

    Fixed stereo playback.

--- spectrogram_paint_ros/scripts/loop_audio.py
+++ spectrogram_paint_ros/scripts/loop_audio.py
@@ -10,10 +10,12 @@ import time
 
 from scipy.io import wavfile
 from spectrogram_paint_ros.msg import Audio
+from threading import Lock
 
 
 class LoopAudio:
     def __init__(self):
+        self.mutex = Lock()
         self.audio = None
         self.audio_msg = None
         self.sub = rospy.Subscriber("audio", Audio, self.audio_callback, queue_size=4)
@@ -26,19 +28,28 @@ class LoopAudio:
             # TODO(lucasw) mutexes, dual buffers
             # This needs to run in separate thread because it is blocking the audio callback,
             # for now sleep
-            print self.audio.shape
-            scikits.audiolab.play(self.audio, fs=self.audio_msg.sample_rate)
+            rospy.loginfo(str(self.audio.shape))
+            self.mutex.acquire()
+            # TODO(lucasw) deepcopy?
+            audio = self.audio
+            sample_rate = self.audio_msg.sample_rate
+            self.mutex.release()
+            scikits.audiolab.play(audio, fs=sample_rate)
             rospy.sleep(0.05)
 
     def audio_callback(self, msg):
         rospy.loginfo("new audio")
-        self.audio_msg = msg
-        self.audio = np.array(msg.data)
+        # self.audio_msg = msg
+        audio = np.array(msg.data)
         # this will throw if the length isn't even
-        if self.audio_msg.stereo:
+        if msg.stereo:
             # TODO(lucasw) need locks to avoid problems with play() above
-            self.audio = np.append(self.audio, msg.data_right)
-            self.audio = self.audio.reshape((2, -1))
+            audio = np.append(audio, msg.data_right)
+            audio = audio.reshape((2, -1))
+        self.mutex.acquire()
+        self.audio = audio
+        self.audio_msg = msg
+        self.mutex.release()
 
 if __name__ == '__main__':
     rospy.init_node('loop_audio')
